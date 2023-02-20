accb5f2c712c3bd954c249f45f998068fb8ff77c
pilasujg <pilasujg@gmail.com>
2023-02-20 12:05:31 +0100

    last changes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,7 @@ import { renderIndex, renderAbout } from "../controllers/index.controllers";
 
 const router = Router();
 
-router.get("/", renderIndex);
+router.get("/home", renderIndex);
 router.get("/about", renderAbout);
 
 export default router;
